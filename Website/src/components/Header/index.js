5193fe805cb7ae66cb3e6e111af2afaa6e9ed0b2
Luiz <luizsaulo@yahoo.com.br>
2022-03-27 18:49:05 -0300

    componente header um pouco mais adiantado, faltando estilização mais apropriada e criação do início da página sobre nós

--- Website/src/components/Header/index.js
+++ Website/src/components/Header/index.js
@@ -1,19 +1,32 @@
-import { Container, Wrapper, Logo, Menu, Link } from "./styles";
+import { Container, Wrapper, Logo, Menu, Page, } from "./styles";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Container>
-      <Wrapper>
+    <div>
+      <Container>
         <Logo>Logotipo</Logo>
-        <Menu>
-          <Link>INÍCIO</Link>
-          <Link>SOBRE NÓS</Link>
-          <Link>PRODUTOS</Link>
-          <Link>ONDEC COMPRAR</Link>
-          <Link>CONTATO</Link>
+        <Menu>          
+          <ul class="nav justify-content-end">
+            <li class="nav-item" className="item">
+              <Link to='/' class="nav-link">INÍCIO</Link>
+            </li>
+            <li class="nav-item">
+              <Link to='sobre' class="nav-link">SOBRE NÓS</Link>
+            </li>
+            <li class="nav-item">
+              <Link to='/produtos' class="nav-link">PRODUTOS</Link>
+            </li>
+            <li class="nav-item">
+              <Link to='onde-comprar' class="nav-link">ONDE COMPRAR</Link>
+            </li>
+            <li class="nav-item">
+              <Link to='contato' class="nav-link">CONTATO</Link>
+            </li>
+          </ul>         
         </Menu>
-      </Wrapper>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
