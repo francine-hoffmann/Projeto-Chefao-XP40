3e7925227fdbf66e7b2de1164b13d3421f925278
Larissa Lopes <larissasantos.lopes@hotmail.com>
2022-03-28 14:31:12 -0300

    Criação da pagina Produtos

--- Website/src/App.js
+++ Website/src/App.js
@@ -2,6 +2,7 @@ import Routes from './Routes';
 import GlobalStyles from './assets/styles/Global'
 import Sobre from './pages/Sobre';
 import Home from './pages/Home'
+import Produtos from './pages/Produtos'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <GlobalStyles />      
       <Routes />
       <Home/>
+      <Sobre/>
+      <Produtos/>
     </div>
   );
 }
--- Website/src/pages/Home/index.js
+++ Website/src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import Bolinhas from "../../components/Bolinhas";
 import MultiCarrossel from "../../components/Carrossel2";
 import img from "../../assets/image/modelo.jpeg"
-import { imagem, img } from './style';
+import { imagem1 } from './style';
 
 
 function Home (){
     return(
         <main>
-            <Bolinhas imagem={img} variante={imagem} />
+            <Bolinhas imagem={img} variante={imagem1} />
             <a href="/" nome="Aromatizantes" > Aromatizantes </a>
 
             <MultiCarrossel/>
