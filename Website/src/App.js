1a14a2baa1b478f38b1a634315d49d40475a154e
Larissa Lopes <larissasantos.lopes@hotmail.com>
2022-03-28 06:24:11 -0300

    Adicionado Componentes do Carrossel de Multiplos Items e Inicio da Criação do componente das Bolinhas

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,12 +1,14 @@
 import Routes from './Routes';
 import GlobalStyles from './assets/styles/Global'
 import Sobre from './pages/Sobre';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />      
       <Routes />
+      <Home/>
     </div>
   );
 }
---
+++ Website/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import MultiCarrossel from "../../components/Carrossel2";
+
+
+function Home (){
+    return(
+        <MultiCarrossel/>
+    );
+
+}
+
+export default Home
