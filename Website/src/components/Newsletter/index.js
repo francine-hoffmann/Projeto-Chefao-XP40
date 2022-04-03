dce406d8b09f5f8234ab33de5ace258ebefe7f0e
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-03 19:12:18 -0300

    adicionando newsletter

--- Website/src/components/Newsletter/index.js
+++ Website/src/components/Newsletter/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './newsletter.css';
+
 
 function Newsletter() {
     return (
         <div>
-            <h1>Hello</h1>
+            <section className='newsletter'>
+                <h3> Cadastre-se e tenha sempre as últimas
+                    atualizações dos produtos Natugama.
+                </h3>
+                <p>Prometemos que será periódica. Não vamos encher
+                    caixa de e-mail á toa.
+                </p>
+                <form method='POST'>
+                    Seu nome
+                    <input type='text' required></input>
+                    Seu e-mail
+                    <input type='email' required></input>
+                    <br></br>
+                    <button>Cadastar</button>
+                </form>
+            </section>
         </div>
     )
         
