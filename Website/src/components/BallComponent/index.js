e7629385f7d71e5a00192dafd67bb9645a8a1c0f
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-04 23:05:26 -0300

    Adicionado carregamento de categorias na homepage, integração com banco de dados de categorias

--- Website/src/components/BallComponent/index.js
+++ Website/src/components/BallComponent/index.js
@@ -1,19 +1,28 @@
-import { Small, Medium, Large} from './style';
+import { Small, Medium, Large, Label} from './style';
 
 function BallComponent ({image, label, size, link}){
     switch(size){
         case "small":
             return (
-                <a href={link}><Small src={image} alt={label} /></a>  
+                <a href={link}>
+                    <Small src={image} alt={label} />
+                    <Label>{label}</Label>
+                </a>  
             )
         default:
         case "medium":
             return (
-                <a href={link}><Medium src={image} alt={label} /></a>  
+                <a href={link}>
+                    <Medium src={image} alt={label} />
+                    <Label>{label}</Label>
+                </a>  
             )
         case "large":
             return (
-                <a href={link}><Large src={image} alt={label} /></a>  
+                <a href={link}>
+                    <Large src={image} alt={label} />
+                    <p>{label}</p>
+                </a>  
             )
     }
 
