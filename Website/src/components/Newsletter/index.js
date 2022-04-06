9aca314815338a6d9e0293d6d6e7c1ba4eee074d
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-06 17:45:14 -0300

    feita alteração de logo social, ajustes de espaçamentos, e finalizando o newsletter

--- Website/src/components/Newsletter/index.js
+++ Website/src/components/Newsletter/index.js
@@ -4,24 +4,29 @@ import './newsletter.css';
 
 function Newsletter() {
     return (
-        <div>
-            <section className='newsletter'>
-                <h3> Cadastre-se e tenha sempre as últimas
-                    atualizações dos produtos Natugama.
-                </h3>
-                <p>Prometemos que será periódica. Não vamos encher
-                    caixa de e-mail á toa.
-                </p>
-                <form method='POST'>
+        <div className="newsletter">
+        <section>
+            <h2>
+                Cadastre-se e tenha sempre as últimas
+                atualizações dos produtos Natugama.
+            </h2>
+            <h5>
+                Prometemos que será periódica. Não vamos encher
+                sua caixa de e-mail á toa.
+            </h5>
+            <div className="formulario">
+                <form method="POST">
                     <p>Seu nome</p>
-                    <input type='text' required></input>
+                    <input type="text" required></input>
+                    <br></br><br></br>
                     <p>Seu e-mail</p>
-                    <input type='email' required></input>
-                    <br></br>
-                    <button>Cadastar</button>
-                </form>
-            </section>
-        </div>
+                    <input type="email" required></input>
+                    <br></br><br></br>
+                    <button>Cadastrar</button> 
+                </form>                
+            </div>                 
+        </section>
+    </div>
     )
         
         
