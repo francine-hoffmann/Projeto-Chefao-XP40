c7269d53709d606a519cd666ceb50def9e8be217
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-07 16:43:45 -0300

    Adicionado componente de avaliação

---
+++ Website/src/components/RatingComponent/index.js
@@ -0,0 +1,27 @@
+import { Container, Button, Message, CheckoutMore, Column, ClientName } from './styles';
+import BallComponent from "../BallComponent";
+import { Rating } from 'react-simple-star-rating';
+
+function RatingComponent ({productName, productImage, clientName, rating, message}){
+    return (
+        <Container>
+            <Column>
+                <div>
+                    <h4>{productName}</h4>
+                    <BallComponent image={productImage} size="small"/>
+                </div>
+            </Column>
+            <Column>
+                <div>
+                    <Rating allowHover={false} initialValue={5} fillColor={'#90BF62'} readonly></Rating>
+                    <ClientName>{clientName}</ClientName>
+                    <Message>"{message}"</Message>
+                    <CheckoutMore>Mais comentários sobre o porquê acho o produto incrível.</CheckoutMore>
+                    <Button>Confira &gt;</Button>
+                </div>
+            </Column>
+        </Container>  
+    )
+}
+
+export default RatingComponent 
