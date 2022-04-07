979c959355261cd144d3418e5db9517d7c404412
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-07 10:52:25 -0300

    Atualização de fontes do site

--- Website/src/assets/styles/Global.js
+++ Website/src/assets/styles/Global.js
@@ -6,6 +6,12 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+    
+    a {
+        text-decoration: none;
+        color: white;
     }
 
 `;
