3b21614f40a7ac0dccc7ccae8bba12276676faa1
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-07 17:23:28 -0300

    Adicionada integração com backend para cadastro em newsletter

--- Website/src/components/Newsletter/index.js
+++ Website/src/components/Newsletter/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
-import { Container, Section, H2Title, H5Title, Formulario, FormInput, FormLabel, FormButton } from "./styles";
+import { Container, H2Title, H5Title, Formulario, FormInput, FormLabel, FormButton } from "./styles";
+import { useState } from "react";
 
 function Newsletter() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(JSON.stringify({
+            nome: name,
+            email: email
+        }));
+        try {
+          let res = await fetch("/newsletters", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nome: name,
+                email: email
+            }),
+          });
+          if (res.status === 200 || res.status === 201) {
+            setName("");
+            setEmail("");
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
     return (
         <Container>
             <section>
@@ -14,20 +44,17 @@ function Newsletter() {
                     sua caixa de e-mail á toa.
                 </H5Title>
                 <Formulario>
-                    <form method="POST">
+                    <form method="POST" onSubmit={handleSubmit}>
                         <FormLabel>Seu nome</FormLabel>
-                        <FormInput type="text" required></FormInput>
+                        <FormInput type="text" required onChange={(e) => setName(e.target.value)} value={name}></FormInput>
                         <FormLabel>Seu e-mail</FormLabel>
-                        <FormInput type="email" required></FormInput>
+                        <FormInput type="email" required onChange={(e) => setEmail(e.target.value)} value={email}></FormInput>
                         <FormButton>Cadastrar</FormButton> 
                     </form>                
                 </Formulario>                 
             </section>
         </Container>
     )
-        
-        
-
 }
 
 export default Newsletter;
