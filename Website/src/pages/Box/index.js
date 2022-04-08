3025946a315263e5978853f3ca9d844be71f6420
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-08 10:59:15 -0300

    Correção de imagens na tela box

--- Website/src/pages/Box/index.js
+++ Website/src/pages/Box/index.js
@@ -9,50 +9,50 @@ function Box() {
     }
   return (
     <Container>
-      <Imagem src="\images\box.png" />
+      <Imagem src="\images\box\box.png" />
         <ComoFunciona>
-            <Imagem src="\images\Como-funciona.png" alt="Como funciona" />
+            <Imagem src="\images\box\Como_funciona.png" alt="Como funciona" />
         </ComoFunciona>
 
         <Menu1>
             <Stamp>
                 <SubTitulo>Descreva</SubTitulo>
-                <Imagem src="\images\folha2.png" />
+                <Imagem src="\images\box\folha2.png" />
                 <Texto>Preciso saber um pouco sobre vocês.Conte-me o tipo de pele, cabelo e preferências.</Texto>
             </Stamp>
 
             <Stamp>
                 <SubTitulo>Escolha</SubTitulo>
-                <Imagem src="\images\mulher2.png" />
+                <Imagem src="\images\box\mulher2.png" />
                 <Texto>Veja as minhas opções de plano e assine o que fazer mais sentido ao seu caso.</Texto>
             </Stamp>
 
             <Stamp>
                 <SubTitulo>Receba</SubTitulo>
-                <Imagem src="\images\caixa2.png" />
+                <Imagem src="\images\box\caixa2.png" />
                 <Texto>A melhor hora: receba a box, se surpreenda com os meus produtos e sinta-se bem ao usá-los.</Texto>
             </Stamp>            
         </Menu1>
 
         <OqueVem>
-            <Imagem src="\images\OqueVem.png" alt='O que vem' />
+            <Imagem src="\images\box\O_que_Vem.png" alt='O que vem' />
         </OqueVem>
 
         <Menu2>
             <Stamp2>
-                <Imagem src="\images\potinho.png" />
+                <Imagem src="\images\box\potinho.png" />
                 <Texto>Produtos naturais <br/> diversificados para: rosto <br/> cabelo, corpo, etc.</Texto>
             </Stamp2>
             <Stamp2>
-                <Imagem src="\images\sabonete.png" />
+                <Imagem src="\images\box\sabonete.png" />
                 <Texto>Todos cuidadosamente <br/> selecionados com base no <br/> seu perfil.</Texto>
             </Stamp2>
             <Stamp2>
-                <Imagem src="\images\escova.png" /><br/>
+                <Imagem src="\images\box\escovas.png" /><br/>
                 <Texto>Acessório surpresa porque <br/> sou grata pela sua <br/> preferência.</Texto>
             </Stamp2>
             <Stamp2>
-                <Imagem src="\images\revista.png"/>
+                <Imagem src="\images\box\revista.png"/>
                 <Texto>Revista Natugama, onde te <br/> dou dicas de cuidados <br/> sustentáveis e conscientes.</Texto>
 
             </Stamp2>
