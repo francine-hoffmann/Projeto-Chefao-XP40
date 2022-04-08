e0c1607be8a4bf20b6319784aae7c7aacfc4bf9c
Larissa Lopes <larissasantos.lopes@hotmail.com>
2022-04-07 23:10:56 -0300

    Alterado componentes pagina sobre

---
+++ src/pages/Contato/Contato.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import * as S from "./style"
+
+  function Contato() {
+  const [nome, setNome] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [email, setEmail] = useState("")
+  const [mensagem, setMensagem] = useState("")
+  function adicionarContato (event){ 
+    event.preventDefault()
+  
+    const body = {
+      nome,
+      telefone,
+      email,
+      mensagem,
+    }
+    console.log(body)
+  
+  }
+     
+  return (
+    <S.Container>
+      <div className='banner'> </div>
+      <S.Conteudo>
+        <article className='section-contato'>
+          <h3>
+            CONTATO
+          </h3>
+          <p>
+            Se deseja entrar em contato, seja para pedir ajuda, esclarecimento ou mais informações sobre os produtos, tenho várias opções para você:
+          </p>
+        </article>
+        <section className='contend-infos'>
+          <div className='section-infos'>
+          <article className='infos'>
+            <span className='bolinha'> <img src='/images/localizacao.png'alt='icone localização'/></span>
+            <div>
+              <h3>Localização</h3>
+              <p>Rua Tal. 1234 - Complemento - Gama Academy - São Paulo CEP 00000-000</p>
+              </div>
+          </article>
+          <article className='infos'>
+            <span className='bolinha'><img src='/images/phone.png'alt='icone de telefone'/></span>
+            <div>
+              <h3>Atendimento</h3>
+              <p>Central de atendimento: (00) 0000-0000 <br/> WhatsApp: (00) 0000-0000</p>
+              </div>
+          </article>
+          <article className='infos'>
+            <span className='bolinha'><img src='/images/person.png'alt='icone de personagem'/></span>
+            <div>
+              <h3>Redes Natugama</h3> 
+              <div className='bolinhas'>
+              <span className='bolinha'> <img src='/images/instagram.png'alt='icone do instagram'/></span>
+              <span className='bolinha'> <img src='/images/facebooklogo.png'alt='icone do instagram'/></span>
+              <span className='bolinha'> <img src='/images/WhatsApp.png'alt='icone do instagram'/></span>
+              </div>
+              </div>
+          </article>
+          </div>
+          <S.Form>
+          <h3>Formulário de Contato
+          </h3>
+          <label>Nome</label>
+          <input type="text" value = {nome} 
+            onChange = {(event) => setNome(event.target.value)}
+          />
+          <label>Telefone</label>
+          <input type="text" value = {telefone} 
+            onChange = {(event) => setTelefone(event.target.value)}/>
+          <label>E-mail</label>
+          <input type="text" value = {email} 
+            onChange = {(event) => setEmail(event.target.value)}/>
+          <label>Mensagem</label>
+          <textarea value = {mensagem} 
+            onChange = {(event) => setMensagem(event.target.value)}> </textarea >
+          <input type="button" value="Enviar" 
+          
+          onClick={adicionarContato} />
+          </S.Form>
+        </section>
+      </S.Conteudo>
+    </S.Container>
+
+  )
+}
+
+export default Contato
+
